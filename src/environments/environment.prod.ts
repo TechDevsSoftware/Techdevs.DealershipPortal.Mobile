cb41ee8e0fbd69ee833a9356bb01f65c32df7467
Steve Kent <stevekent55@me.com>
2018-08-07 20:48:04 +0100

    Updated accountServer link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  accountServer: "https://techdevs-usermanagement.azurewebsites.net",
+  accountServer: "https://techdevs-accounts.azurewebsites.net",
   identityServer: "https://techdevs-identityserver.azurewebsites.net",
   clientAddress: "https://techdevs-dpmobile.netlify.com",
   identityClientId: "spa",
